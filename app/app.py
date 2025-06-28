2a66bfcb24ed9182c060411614fd293c7746aedc
Marcel Higor quinteiro Araujo <117388829+marcelquin@users.noreply.github.com>
2025-06-28 18:57:42 -0400

    sistema

---
+++ app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify, render_template
+from ia_Treinamento import fazer_busca
+
+app = Flask(__name__)
+
+# Rota para a página inicial
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Rota da API
+@app.route('/api/busca', methods=['POST'])
+def busca_api():
+    data = request.get_json()
+    pergunta = data.get('pergunta')
+    resultado = fazer_busca(pergunta)
+    return jsonify({'resposta': resultado})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
